71cf5d0eb5cdc3949d5aab99f7af2c9da56ee043
LucasAlencar <lucas.linard@outlook.com>
2023-03-02 13:04:26 -0300

    colors based on prop type

--- src/components/Text/styles.ts
+++ src/components/Text/styles.ts
@@ -1,18 +1,41 @@
-import styled from 'styled-components/native';
+import styled, {css} from 'styled-components/native';
 
 export interface TextProps {
-    color?: string;
-    fontWeight?: string;
-    fontSize?: string;
-    width?: string;
-    marginRight?: string;
+  color?: string;
+  fontWeight?: string;
+  fontSize?: string;
+  width?: string;
+  marginRight?: string;
+  type?: 'primary' | 'secondary' | 'option';
 }
 
+const TextColor = ({type}: TextProps) => {
+  switch (type) {
+    case 'primary':
+      return css`
+        ${props => props.theme.COLORS.TEXT};
+      `;
+    case 'secondary':
+      return css`
+        ${props => props.theme.COLORS.TEXT_SECONDARY};
+      `;
+
+    case 'option':
+      return css`
+        ${props => props.theme.COLORS.TEXT_OPTION};
+      `;
+
+    default:
+      return css`
+        ${props => props.theme.COLORS.TEXT};
+      `;
+  }
+};
 
 export const Text = styled.Text<TextProps>`
-    color: ${props => props.color || props.theme.COLORS.TEXT};
-    font-weight: ${props => props.fontWeight || 'normal'};
-    font-size: ${props => props.fontSize || props.theme.SIZES.FONT}px;
-    width: ${props => props.width  || 'auto'};
-    margin-right: ${props => props.marginRight || '0'}px;
-`
+  font-weight: ${props => props.fontWeight || 'normal'};
+  font-size: ${props => props.fontSize || props.theme.SIZES.FONT}px;
+  width: ${props => props.width || 'auto'};
+  margin-right: ${props => props.marginRight || '0'}px;
+  color: ${props => TextColor(props)};
+`;
